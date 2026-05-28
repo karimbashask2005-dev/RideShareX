b534b74e9e91e83e8d56dac449196ed56444942f
karimbashask2005-dev <karimbasha.sk2005@gmail.com>
2026-05-28 23:05:11 +0530

    chore: configure base path for production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/RideShareX/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -18,4 +19,4 @@ export default defineConfig({
       }
     }
   }
-})
+}))
